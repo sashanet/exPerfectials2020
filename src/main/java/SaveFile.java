5db9aa679975e04bdcb111803c13421ca2603929
rhrytsiv <hrytsiv.roman@gmail.com>
2020-02-20 20:35:45 +0200

    dasdasd

--- src/main/java/SaveFile.java
+++ src/main/java/SaveFile.java
@@ -3,6 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Oleksandr Buryk
@@ -15,19 +16,21 @@ public class SaveFile {
 
             writer.append(String.valueOf(result.getLibrariesToBooks().size())).append("\n");
 
-            for(Map.Entry<Integer, List<Integer>>  entry: result.getLibrariesToBooks().entrySet()) {
+            for (Map.Entry<Integer, List<Integer>> entry : result.getLibrariesToBooks().entrySet()) {
 
-                writer.append(String.valueOf(entry.getKey())).append(" ").append(String.valueOf())
+                writer.append(String.valueOf(entry.getKey()))
+                      .append(" ")
+                      .append(String.valueOf(entry.getValue().size()))
+                      .append("\n");
 
-                writer.append(String.valueOf(entry.getKey()) + " " + );
+                writer.append(entry.getValue().stream().map(String::valueOf).collect(Collectors.joining(" ")))
+                      .append("\n");
 
             }
-
-
-
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
+
 }

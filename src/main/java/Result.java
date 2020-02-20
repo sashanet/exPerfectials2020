6656a575415fcbe5ec03401983c92fc1cfd55b88
rhrytsiv <hrytsiv.roman@gmail.com>
2020-02-20 20:33:12 +0200

    dasdasd

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,2 +1,19 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Result {
+
+    private LinkedHashMap<Integer, List<Integer>> librariesToBooks;
+
+    public LinkedHashMap<Integer, List<Integer>> getLibrariesToBooks() {
+
+        return librariesToBooks;
+    }
+
+    public void setLibrariesToBooks(LinkedHashMap<Integer, List<Integer>> librariesToBooks) {
+
+        this.librariesToBooks = librariesToBooks;
+    }
+
 }

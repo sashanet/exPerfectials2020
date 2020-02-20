c09b7e2639a7e1cf4d410ab64569a1f68cfa0dd6
rhrytsiv <hrytsiv.roman@gmail.com>
2020-02-20 20:57:57 +0200

    dasdasd

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -3,6 +3,7 @@ import model.Library;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
@@ -13,16 +14,20 @@ import java.util.stream.Stream;
 
 public class Solver {
 
+    private Set<Integer> processBooks = new HashSet<>();
+
+    private BaseInfo baseInfo;
+
     public Result solve(BaseInfo baseInfo) {
 
-        int daysForScan = baseInfo.days;
+        baseInfo = baseInfo;
         Result result = new Result();
-    LinkedHashMap<Integer, List<Integer>> map = new LinkedHashMap<>();
-    result.setLibrariesToBooks(map);
+        LinkedHashMap<Integer, List<Integer>> map = new LinkedHashMap<>();
+        result.setLibrariesToBooks(map);
 
-        Set<Library> libraries = sort(baseInfo);
+        Set<Library> libraries = sort();
         for (Library lib : libraries) {
-            if (lib.signUpProcess < daysForScan) {
+            if (lib.signUpProcess < baseInfo.days) {
                 addLibrary(map, lib);
             }
         }
@@ -30,11 +35,19 @@ public class Solver {
     }
 
     private void addLibrary(LinkedHashMap<Integer, List<Integer>> result, Library lib) {
-    List<Integer> ts = IntStream.of(lib.booksIds).boxed().collect(Collectors.toList());
-    result.put(lib.id, ts);
+
+        List<Integer> books = IntStream.of(lib.booksIds).boxed().collect(Collectors.toList());
+
+        books.removeAll(processBooks);
+
+        int bookProcessCount = Integer.min(books.size(), lib.booksPerDay * (baseInfo.days - lib.signUpProcess));
+
+        processBooks.addAll(books.subList(0, bookProcessCount));
+
+        result.put(lib.id, books);
     }
 
-    private Set<Library> sort(BaseInfo baseInfo) {
+    private Set<Library> sort() {
 
         TreeSet<Library> set =
                 new TreeSet<>(Comparator.comparingDouble(l -> getLibValue(baseInfo.days, baseInfo.scores, l)));

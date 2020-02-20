d7bd9753423e1f1185f11594bd0c820bad707f66
rhrytsiv <hrytsiv.roman@gmail.com>
2020-02-20 20:44:22 +0200

    dasdasd

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -7,41 +7,49 @@ import java.util.TreeSet;
 
 public class Solver {
 
-  public Result solve(BaseInfo baseInfo) {
+    public Result solve(BaseInfo baseInfo) {
 
-    int daysForScan = baseInfo.days;
-    Result result = new Result();
+        int daysForScan = baseInfo.days;
+        Result result = new Result();
 
-    Set<Library> libraries = sort(baseInfo);
-    for (Library lib: libraries) {
-      if (lib.signUpProcess < daysForScan) {
-        addLibrary(result, lib, baseInfo.scores);
-      }
+        Set<Library> libraries = sort(baseInfo);
+        for (Library lib : libraries) {
+            if (lib.signUpProcess < daysForScan) {
+                addLibrary(result, lib, baseInfo.scores);
+            }
+        }
+        return result;
     }
-    return result;
-  }
 
-  private void addLibrary(Result result, Library lib, int[] scores) {
+    private void addLibrary(Result result, Library lib, int[] scores) {
 
-  }
-
-  private Set<Library> sort(BaseInfo baseInfo){
-    TreeSet<Library> set = new TreeSet<>(Comparator.comparingDouble(l -> getLibValue(baseInfo.days, baseInfo.scores, l)));
-    set.addAll(baseInfo.libraryList);
-    return set;
-  }
-
-  private double getLibValue(int daysForScan, int[] scores, Library l) {
-    int deliveryDays = Integer.min(l.booksIds.length * l.booksPerDay, daysForScan - l.signUpProcess) - l.signUpProcess;
-    int booksValue = countBooksValue(l.booksIds, scores);
-    return 1.0 * booksValue/deliveryDays;
-  }
-
-  private int countBooksValue(int[] booksIds, int[] scores) {
-    int sum = 0;
-    for (int id : booksIds) {
-      sum += scores[id];
     }
-    return sum;
-  }
+
+    private Set<Library> sort(BaseInfo baseInfo) {
+
+        TreeSet<Library> set =
+                new TreeSet<>(Comparator.comparingDouble(l -> getLibValue(baseInfo.days, baseInfo.scores, l)));
+        set.addAll(baseInfo.libraryList);
+        return set;
+    }
+
+    private double getLibValue(int daysForScan, int[] scores, Library l) {
+
+        int deliveryDays =
+                Integer.min(l.booksIds.length / l.booksPerDay, daysForScan - l.signUpProcess) - l.signUpProcess;
+        int booksValue = countBooksValue(l.booksIds, scores, daysForScan - l.signUpProcess);
+        return 1.0 * booksValue / deliveryDays;
+    }
+
+    private int countBooksValue(int[] booksIds, int[] scores, int daysToProcess) {
+
+        int sum = 0;
+        daysToProcess--;
+        for (int i = 0; i < daysToProcess; i++) {
+
+            sum += scores[booksIds[i]];
+        }
+        return sum;
+    }
+
 }
